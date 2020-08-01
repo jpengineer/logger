e518e6fefb3be270ca1962dbd6222e69fd7ecdad
Juan Alejandro Perez Chandia <jalejandro.ingeniero@gmail.com>
2020-08-01 00:05:30 -0400

    Create basic test function - Logger v1.2.A

---
+++ test.go
@@ -0,0 +1,19 @@
+package logger
+
+import (
+	Logger "github.com/jpengineer/logger"
+)
+
+func Test() {
+	var _log, _ = Logger.Start("test.log", "./", Logger.Level.DEBUG)
+	_log.Rotation(40, 5)
+	_log.Statistics(true)
+
+	_log.Critical("github.com/jpengineer/logger --> This is a Critical log test")
+	_log.Info("github.com/jpengineer/logger --> This is a INFO log test")
+	_log.Warn("github.com/jpengineer/logger --> This is a WARNING log test")
+	_log.Error("github.com/jpengineer/logger --> This is a ERROR log test")
+	_log.Debug("github.com/jpengineer/logger --> This is a DEBUG log test")
+
+	_log.Close()
+}
