e473bbc8785979c098a7eb69fef08734a421701d
Juan Alejandro Perez Chandia <jalejandro.ingeniero@gmail.com>
2020-08-01 00:41:14 -0400

    fix test()

--- test.go
+++ test.go
@@ -5,11 +5,11 @@ func Test() {
 	_log.Rotation(40, 5)
 	_log.Statistics(true)
 
-	_log.Critical("github.com/jpengineer/logger --> This is a Critical log test")
-	_log.Info("github.com/jpengineer/logger --> This is a INFO log test")
-	_log.Warn("github.com/jpengineer/logger --> This is a WARNING log test")
-	_log.Error("github.com/jpengineer/logger --> This is a ERROR log test")
-	_log.Debug("github.com/jpengineer/logger --> This is a DEBUG log test")
+	_log.Critical("--> This is a Critical log test")
+	_log.Info("This is a INFO log test")
+	_log.Warn("This is a WARNING log test")
+	_log.Error("This is a ERROR log test")
+	_log.Debug("This is a DEBUG log test")
 
 	_log.Close()
 }
