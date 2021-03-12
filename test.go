96243d7533d5394f081e999d5d1470a18c24dbe3
Juan Alejandro Perez Chandia <jalejandro.ingeniero@gmail.com>
2021-03-11 22:00:50 -0300

    Timestamp setting v1.3.0

--- test.go
+++ test.go
@@ -3,6 +3,7 @@ package logger
 func Test() {
 	_log, _ := Start("test.log", ".", Level.DEBUG)
 	_log.Rotation(40, 5)
+	_log.TimestampFormat(TS.Special)
 
 	_log.Critical("This is a Critical message")
 	_log.Info("This is an Info message")
